a7f84fa851d179c4b2ca03b4f15e0f0ea192ff89
alexwlad3885 <alexwlad3885@yandex.ru>
2024-07-11 21:35:49 +0300

    # Домашнее задание по теме "Генераторные сборки"

---
+++ module_9_3.py
@@ -0,0 +1,10 @@
+# Домашнее задание по теме "Генераторные сборки"
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+print(list(first_result))
+
+second_result = (len(first[i]) == len(second[j]) for i in range(len(first)) for j in range(len(second)) if i == j)
+print(list(second_result))
